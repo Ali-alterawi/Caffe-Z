78a665edaad7849a3d7a0aa2c8145515bef5c799
Ali-alterawi <alielterawe@gmail.com>
2023-03-19 15:53:42 +0300

    first-attempt

--- java.js
+++ java.js
@@ -1,15 +1,28 @@
-let name = prompt("Please enter your name");
+let user_name = prompt("Please enter your name");
 let gender = prompt("Please enter your gender (male or female):");
 
+
+
+
+
+
 if (gender === "male") {
-  alert("Hello Mr " + name);
+  alert("Hello Mr " + user_name);
 } else if (gender === "female") {
-  alert("Hello Ms " + name);
-} else {
-  alert("Hello " + name);
+  alert("Hello Ms " + user_name);
+} 
+while( gender != "male" && gender !="female" ){
+  gender = prompt("please enter your real gender(male or female)")
 }
+
 let drink = prompt("do you want a hot or a cold drink?")
 let drinkName= prompt("name of the drink")
 alert("your drink is getting prepared")
 
-console.log(name + " ordered a " + drink + " "+drinkName+ ".");
+let info = [ user_name , gender ,drink, drinkName ];
+for (let i = 0 ; i < info.length ; i++){
+  console.log(info[i])
+}
+// console.log(info);
+
+// console.log(name + " ordered a " + drink + " "+drinkName+ ".");
